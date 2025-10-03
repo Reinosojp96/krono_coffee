3b2414c49131f5612b540b197293bafe7a1953c6
reinoso1 <juliandresp09@gmail.com>
2025-10-03 13:57:00 -0500

    retoque

--- backend/app/api/v1/endpoints/__init__.py
+++
@@ -1,7 +0,0 @@
-from .endpoints_auth import router as auth
-from .endpoints_users import router as users
-from .endpoints_menu import router as menu
-from .endpoints_orders import router as orders
-from .endpoints_payments import router as payments
-from .endpoints_offers import router as offers
-from .endpoints_updates import router as updates
